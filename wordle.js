a905531f80ef86a635a1c627612ca686bace1a22
Zach Paul <zachpaul319@gmail.com>
2023-01-02 23:17:10 -0500

    can change all colors in a row

--- wordle.js
+++ wordle.js
@@ -1,6 +1,8 @@
 const answer = "hello";
 var guess = [];
 var currentTile = "A1"
+var rows = ["A", "B", "C", "D", "E", "F"];
+var currentRow = rows[0];
 
 function keyPress(e) {
     return e.key;
@@ -38,29 +40,34 @@ function goToPreviousTile(current) {
     }
 }
 
-function isCorrect() {
+function changeTileColor(i, color) {
+    const tiles = document.getElementsByClassName("tile")
+    tiles[i].style.backgroundColor = color;
+    tiles[i].style.color = "white";
 }
 
-function changeTileColors() {
-    const tiles = document.getElementsByClassName("tile");
-
-    for (let i = 0; i < 5; i++) {
+function changeAllTilesInRow() {
+    var i = 0;
+    var interval = setInterval(function() {
         if (guess[i] == answer[i]) {
-            tiles[i].style.backgroundColor = "green";
+            changeTileColor(i, "green");
         } else if (answer.includes(guess[i])) {
-            tiles[i].style.backgroundColor = "gold";
+            changeTileColor(i, "gold");
         } else {
-            tiles[i].style.backgroundColor = "grey";
+            changeTileColor(i, "grey")
         }
-        tiles[i].style.color = "white";
-    }
+
+        if (++i == 5) clearInterval(interval);
+    }, 250)
 }
 
 function addKeyDownListener() {
     document.getElementsByTagName("body")[0].addEventListener("keydown", function() {
         let keyPressed = String(keyPress(window.event));
         
-         if (keyPressed == "Backspace") {
+        if (guess.length == 5 && keyPressed == "Enter") {
+            changeAllTilesInRow();
+        } else if (keyPressed == "Backspace") {
             goToPreviousTile(currentTile);
             removeDisplayedLetter();
             removeLetterFromGuess(keyPressed);
